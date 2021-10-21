c961aea6b1cfbae7356aac33fc28426bf0cf7e42
Marcelo Braz <marcelobraz7@gmail.com>
2021-10-21 14:27:12 -0300

    complete the temperature converter

--- conversor.js
+++ conversor.js
@@ -1,43 +1,42 @@
 //adiciona a seleção de elemento a variável botaoConverter
 let botaoConverter = document.querySelector('#converter')
 
-//adiciona um evento de clique no botaoConverter
-botaoConverter.addEventListener('click', function(e){
-    
-    let celsius = document.querySelector('#celsius').value
-    console.log(celsius)
-
-    let fahrenheit = document.querySelector('#fahrenheit').value
-    console.log(fahrenheit)
-
-    //calculo de teste
-    let cToFahr = celsius * 9 / 5 + 32;
-    console.log(cToFahr)
-
-    //joga o resultado do calculo de teste dentro da div result
-    let result = document.getElementById('result').textContent = `${cToFahr + 'ºC'}`
-
-
-})
-
-
 //função para pegar o valor do option e realizar o teste condicional
 function getSelectValue() {
-        var selectedValue = document.getElementById("list").value;
-        let valor = selectedValue
+  
+  //adiciona um evento de clique no botaoConverter
+  botaoConverter.addEventListener('click', function(e){
     
-        //se o valor for igual a 0
-        if(valor == 0){
-          console.log('ESCOLHA UMA DAS OPÇÕES')
-    
-        //senão se o valor for igual a 1
-        } else if (valor == 1) {
-          console.log('CELSIUS')
-            
+    //adiciona os elementos selecionados em variáveis
+    let valorTemp = document.querySelector('#celsius').value;
+    let selectedValue = document.getElementById("list").value;
+    let valor = selectedValue
+
+      //se o valor for igual a 0 ou vazio
+      if(valor == 0 || valor == ''){
+        alert('ESCOLHA UMA DAS OPÇÕES')
+        
+      //senão se o valor for igual a 1
+      } else if (valor == 1) {
+          //altera o texto do elemento selecionado para Celsius
+          let trocaTexto = document.getElementById('trocatexto').textContent = 'Celsius'
+
+          //conversão de Fahrenheit para Celsius
+          let fToCel = (valorTemp - 32) * 5 / 9;
+          let result = document.getElementById('result').textContent = `${fToCel.toFixed(2) + 'ºC'}`
+
+        
+        //senão se o valor for igual a 2 
+      } else if (valor == 2) {
+          //altera o texto do elemento selecionado para Fahrenheit
+          let trocaTexto = document.getElementById('trocatexto').textContent = 'Fahrenheit'
           
-        //senão o valor for igual a 2
-        }else if(valor == 2) 
-          console.log('Fahrenheit')
+          //conversão de Celsius para Fahrenheit
+          let cToFahr = valorTemp * 9 / 5 + 32;
+          //joga o resultado do calculo dentro da div result
+          let result = document.getElementById('result').textContent = `${cToFahr.toFixed(2) + 'ºF'}`
+      }
+  })
 }
 
 //chamada da função acima
